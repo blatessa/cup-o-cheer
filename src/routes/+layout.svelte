e62513ac27c4da29162d82f41872c3f9b5254322
Brady LaTessa <brady@latessa.net>
2025-12-24 13:08:40 -0500

    added dynamic title based on route.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
     import { onMount } from "svelte";
 	import { page } from "$app/state";
     import { goto } from "$app/navigation";
+
+
 	let { children } = $props();
 
 	let user = null;
@@ -28,7 +30,7 @@
 </script>
 
 <svelte:head>
-	<title>Cup O' Cheer</title>
+	<title>{(page.data.title) ?? ''} - Cup O' Cheer</title>
 	<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☕</text></svg>">
 </svelte:head>
 
